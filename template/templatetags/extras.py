1895afe83d49268060198fb41be3cb4e77c03c2a
atymchenko <artem.tym@gmail.com>
2019-07-24 22:20:50 +0200

    filter_start

--- template/templatetags/extras.py
+++ template/templatetags/extras.py
@@ -1,1 +1,11 @@
 from django import template
+
+register = template.Library()
+
+@register.filter(name = 'inc')
+def inc(value, arg):
+    pass
+
+@register.filter
+def devision(value, arg1, arg2):
+    pass
